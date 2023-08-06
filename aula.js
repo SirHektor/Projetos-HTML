973b424dd337991eb6bac13d34e0dc4952bf3bd4
SirHektor <133364793+SirHektor@users.noreply.github.com>
2023-08-06 11:26:27 -0300

    Initial commit

---
+++ aula.js
@@ -0,0 +1,10 @@
+console.log(Math.random());
+console.log(Math.random()*20);
+console.log(Math.random(7 - 3 + 1) +3);
+
+//arredonda
+console.log(Math.round(3.4)); 
+console.log(Math.round(3.9));
+
+console.log(Math.floor(3.9));//baixo      
+console.log(Math.ceil(3.1));//cima
